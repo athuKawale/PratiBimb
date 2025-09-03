5efb28b87cc92e9bcd59137d5ef593b7a17b55ed
Jagrut <202702863+jagrut-thakare@users.noreply.github.com>
2025-09-03 21:37:30 +0530

    Fix server port in uvicorn run command from 8002 to 8000

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ app.include_router(videoswap.router)
 if __name__ == "__main__":
     try:
         # Keep reload=True only in Devlopement as workers flag is ignored in that case. In Production remove reload=True.
-        uvicorn.run("main:app", host="0.0.0.0", port=8002, reload=True, workers=workers)
+        uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, workers=workers)
 
     except Exception as e:
 
